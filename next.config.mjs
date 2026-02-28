abd3df10c07983f2c2de10dcc4b945c7c4470949
Souleimane <souleimanehadbi@gmail.com>
2026-02-28 12:27:26 +0100

    chore: configure nextjs for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+};
 
 export default nextConfig;
